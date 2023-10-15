e2dc19df5630e1ce0550ea0f8e98fac77da1e1c3
Joe Clark <weirdlookingjay@live.com>
2023-10-14 22:30:10 -0400

    Created Navbar and Dark/Light Theme

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -11,7 +11,13 @@ export const Heroes = () => {
             src="/documents.png"
             alt="Documents"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/documents-dark.png"
+            alt="Documents"
+            fill
+            className="object-contain hidden dark:block"
           />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
@@ -19,7 +25,13 @@ export const Heroes = () => {
             src="/reading.png"
             alt="Reading"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/reading-dark.png"
+            alt="Reading"
+            fill
+            className="object-contain hidden dark:block"
           />
         </div>
       </div>
--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -11,7 +11,20 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" alt="Logo" height="40" width="40" />
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        height="40"
+        width="40"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        alt="Logo"
+        height="40"
+        width="40"
+        className="hidden dark:block"
+      />
       <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
