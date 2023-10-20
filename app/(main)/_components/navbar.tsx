6dd5ad6de5d77e8093cef0131b2c3880fba0d2d1
Joseph Clark <weirdlookingjay@live.com>
2023-10-20 15:42:37 -0400

    Created functionalty to Edit a documen's title

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import { Title } from "./title";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -19,7 +20,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   });
 
   if (document === undefined) {
-    return <p>Loadding...</p>;
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center ">
+        <Title.Skeleton />
+      </nav>
+    );
   }
 
   if (document === null) {
@@ -36,7 +41,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between w-full">Navbar</div>
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
       </nav>
     </>
   );
