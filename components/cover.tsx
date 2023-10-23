e45f5f1cd682cd69277db14a5f2d07b41657eb11
Joseph Clark <weirdlookingjay@live.com>
2023-10-23 15:17:38 -0400

    Added a Remove/Replace cover image feature

--- components/cover.tsx
+++ components/cover.tsx
@@ -2,6 +2,13 @@
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CoverImageProps {
   url?: string;
@@ -9,6 +16,16 @@ interface CoverImageProps {
 }
 
 export const Cover = ({ url, preview }: CoverImageProps) => {
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const params = useParams();
+
+  const onRemove = () => {
+    removeCoverImage({
+      id: params.documentId as Id<"documents">,
+    });
+  };
+
   return (
     <div
       className={cn(
@@ -18,6 +35,29 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     >
       {!!url && <Image src={url} fill alt="cover" className="object-cover" />}
+
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X />
+            Remove
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
